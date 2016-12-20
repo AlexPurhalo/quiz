f255c2a80481d7019079a2188b677c37843e63c6
AlexPurhalo <alexpurhalo@gmail.com>
2016-12-20 17:54:47 +0200

    header-styling

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,20 @@
 // Node modules import
 import React, { Component } from 'react';
 
+// Component import
+import Header from './header';
+
 // Layout component
 export default class App extends Component {
     render() {
         return (
             <div className="app">
-                HEADER
+                <Header />
                 <div className="container">
                     <h3>
                         {this.props.children}
                     </h3>
                 </div>
-                FOOTER
             </div>
         );
 
