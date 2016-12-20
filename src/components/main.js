122e969e76f290527d05319955de67d0e3e852e5
AlexPurhalo <alexpurhalo@gmail.com>
2016-12-20 19:16:42 +0200

    data-fetching-with-axios

--- src/components/main.js
+++ src/components/main.js
@@ -1,12 +1,19 @@
 // Node modules import
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { fetchQuestion } from '../actions/questions';
 
 // Components import
 import ActivityLog from './main/activity-log';
 import QuestionInfo from './main/question-info';
 
 // Main page
-export default class Main extends Component {
+class Main extends Component {
+    componentWillMount() {
+        this.props.fetchQuestion();
+    }
+
     render() {
         return (
             <div className="main-page">
@@ -17,3 +24,4 @@ export default class Main extends Component {
     }
 }
 
+export default connect(null, { fetchQuestion })(Main)
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
+import reduxThunk from 'redux-thunk';
 
 // Components import
 import routes from './routers';
@@ -15,7 +16,7 @@ import reducers from './reducers/index.js';
 import '../styles/index.scss';
 
 // Store definition with Middleware applying and Rendering of React Document Object Model
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
