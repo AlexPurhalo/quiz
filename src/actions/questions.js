b9e05294f0d4cffe2defdd8e31023528f384af96
AlexPurhalo <alexpurhalo@gmail.com>
2016-12-27 23:42:15 +0200

    changed-api

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -17,13 +17,14 @@ import { strToObjsArr, randomizeItems } from '../functions/answer-transformation
 // Receives a random question
 export function fetchQuestion() {
     return function(dispatch) {
-        return axios.get('http://jservice.io/api/random')
+        return axios.get('https://qukz-back-2.herokuapp.com/questions/random')
             .then(response => {
-                const questionObj = response.data[0];
+                console.log(response.data);
+                const questionObj = response.data;
                 const data = {
                     id: questionObj.id,
                     answer: questionObj.answer,
-                    description: questionObj.question,
+                    description: questionObj.description,
                     category: questionObj.category.title,
                     transformedAnswer: randomizeItems(strToObjsArr(questionObj.answer))
                 };
